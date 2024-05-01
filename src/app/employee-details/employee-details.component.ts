a787a85d4a393d66d5be260b64c834f9920aa188
Ritik Vishwakarma <ritikvishwakarma739@gmail.com>
2024-05-01 22:09:08 +0530

    fourth

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute, Route } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrl: './employee-details.component.css'
+  styleUrls: ['./employee-details.component.css'] // <-- Corrected property name
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements OnInit {
+  id: number | undefined;
+  employee!: Employee;
 
+  constructor(private route: ActivatedRoute, private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'] as number;
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    });
+  }
 }
